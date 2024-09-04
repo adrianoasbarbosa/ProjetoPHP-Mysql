3ff3676254328b971616c592da0c210dda789e1a
adrianoasbarbosa <adrianodevsistemas@gmail.com>
2024-09-03 21:40:30 -0300

    atualização listar

--- admin/equipe/listar.php
+++ admin/equipe/listar.php
@@ -1,28 +1,28 @@
-<h3>Lista de Passageiros</h3>
-<a class="btn btn-outline-primary float-right" href="?p=passageiro/salvar">Add</a>
+<h3>Lista de Equipes</h3>
+<a class="btn btn-outline-primary float-right" href="?p=equipe/salvar">Add</a>
 <br><br>
 <table class="table">
     <thead class="thead-dark">
         <tr>
             <th scope="col">#</th>
-            <th scope="col">Nome</th>
-            <th scope="col">CPF</th>
+            <th scope="col">Nome Equipe</th>
+            <th scope="col">Nr Membros</th>
             <th>Opções</th>
         </tr>
     </thead>
     <tbody>
         <?php
-        include_once '../class/Passageiro.php';
-        $passageiro = new Passageiro();
-        $dados = $passageiro->listar(NULL);
+        include_once '../class/Equipe.php';
+        $eq = new Equipe();
+        $dados = $eq->listar(NULL);
 
         if (!empty($dados)) {
             foreach ($dados as $mostrar) {
         ?>
                 <tr>
                     <th scope="row"><?= $mostrar['id'] ?></th>
-                    <td><?= $mostrar['nome'] ?></td>
-                    <td><?= $mostrar['cpf'] ?></td>
+                    <td><?= $mostrar['nome_equipe'] ?></td>
+                    <td><?= $mostrar['nr_membros'] ?></td>
                     <td>
                         <a href="" class="btn btn-primary" title="editar registro">
                             <i class="bi bi-pencil-square"></i>
